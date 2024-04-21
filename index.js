95e85e917a66e5e2eabca4f0291416ac065a1d01
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-04-21 18:53:22 +0530

    allow any were

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ MongodbConnection()
     .then(() => {
         // MongoDB connection successful, set up middleware and routes
         app.use(cors({
-            origin: 'http://localhost:3000',
+            origin: '*',
             credentials: true,
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
 
