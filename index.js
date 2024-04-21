370b9bac3e58ae93372288f9843d3438fad9a6c1
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-04-21 18:57:46 +0530

    change the origin

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ MongodbConnection()
     .then(() => {
         // MongoDB connection successful, set up middleware and routes
         app.use(cors({
-            origin: '*',
+            origin: 'https://frontendblog-nu.vercel.app',
             credentials: true,
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
 
